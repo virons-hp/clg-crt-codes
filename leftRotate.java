8659f5e29ea0e81fd6f1c0bdc758c0fe9e1dcc05
virons-hp <taknikwala@gmail.com>
2026-06-10 14:18:21 +0530

    rotate element to the left in arrya

---
+++ leftRotate.java
@@ -0,0 +1,13 @@
+public class leftRotate {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3,4,5,6,7};
+        int n = arr.length;
+        int temp = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            arr[i - 1] = arr[i];
+        }
+        arr[n - 1] = temp;
+        System.out.println(java.util.Arrays.toString(arr));
+    }
+}
