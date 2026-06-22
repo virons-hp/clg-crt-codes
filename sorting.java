0b19c8f96d6e288bf1fab3dfc43d0e77fc8b2f6a
virons-hp <taknikwala@gmail.com>
2026-06-22 15:29:13 +0530

    sorting

---
+++ sorting.java
@@ -0,0 +1,15 @@
+public class sorting {
+     
+    public static int red(){
+        int a = 0;
+        return a;
+    }
+     public static void main(String[] args) {
+        int[] arr= {5,4,3,2,1};
+        int result = red();
+        System.out.println(result);
+        System.out.println(a);
+ 
+       
+     }
+}
