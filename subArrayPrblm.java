5711b8eaa38564c7211909498c224f7cbcb5bab0
virons-hp <taknikwala@gmail.com>
2026-06-25 14:47:39 +0530

    kadane's algorithm

---
+++ subArrayPrblm.java
@@ -0,0 +1,13 @@
+class subArrayPrblm {
+    int maxSubarraySum(int[] arr) {
+        int currentSum = arr[0];
+        int maxSum = arr[0];
+
+        for (int i = 1; i < arr.length; i++) {
+            currentSum = Math.max(arr[i], currentSum + arr[i]);
+            maxSum = Math.max(maxSum, currentSum);
+        }
+
+        return maxSum;
+    }
+}
